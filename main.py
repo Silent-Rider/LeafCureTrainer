2fc96dcf34958028bbdc0ae95fde561d967d87db
Silent-Rider <silent.30.rider.10@gmail.com>
2026-06-01 02:50:03 +0700

    Finishing

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from pathlib import Path
-
-from evaluate.evaluate import evaluate_binary_models, evaluate_categorical_models
-from process.seg_process import segment_and_apply_masks
-
-
 def main():
-    segment_and_apply_masks(base_input_dir=Path(r"dataset\classify\binary_test\grape_binary\Grape___diseases"),
-                            base_output_dir=Path(r"C:\Users\Silent Rider\Desktop\test\mask"),
-                            mask_output_dir=Path(r"C:\Users\Silent Rider\Desktop\test\masked"),
-                            model_name='spot_seg_final')
+    pass
 
 if __name__ == "__main__":
     main()
