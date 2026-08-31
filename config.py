ee3167cb2cf579184dd189c823a65e633e01e45c
Silent-Rider <silent.30.rider.10@gmail.com>
2026-08-31 15:59:52 +0700

    Подготовил apple_categorical

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 PLANTS = {'apple','corn','grape','potato','tomato'}
 TASK_NAME = 'classify'
-CLASSIFY_TYPE = 'binary'
+CLASSIFY_TYPE = 'categorical'
 IMAGE_SIZE = (256, 256)
 BATCH_SIZE = 32
 EPOCHS = 30
