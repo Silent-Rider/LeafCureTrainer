2c45822dab6a31623775899b880a155552f6b003
Silent-Rider <silent.30.rider.10@gmail.com>
2026-08-31 15:32:03 +0700

    Подготовил программу для переобучения бинарной модели яблони (без кедровой ржавчины)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 PLANTS = {'apple','corn','grape','potato','tomato'}
 TASK_NAME = 'classify'
-CLASSIFY_TYPE = 'categorical'
+CLASSIFY_TYPE = 'binary'
 IMAGE_SIZE = (256, 256)
 BATCH_SIZE = 32
 EPOCHS = 30
